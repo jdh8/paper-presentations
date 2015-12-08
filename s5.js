d33a9190c892a9e49919552d2f0eecb76e41ff58
Chen-Pang He <jdh8@ms63.hinet.net>
2015-12-09 02:08:11 +0800

    Imporve interactivity

--- s5.js
+++ s5.js
@@ -2,15 +2,9 @@
 
 (function()
 {
-	function Nothing() {}
-
-	function Maybe(x)
-	{
-		return x == null ? Nothing : x;
-	}
-
 	var slides = document.getElementsByClassName("slide");
 	var active = 0;
+	var play;
 
 	function switchTo(index)
 	{
@@ -26,12 +20,13 @@
 		active = index;
 	}
 
-	function prev() { switchTo(active - 1) }
-	function next() { switchTo(active + 1) }
+	function noop() {}
+	function prev() { play(active - 1) }
+	function next() { play(active + 1) }
 
 	function toggle()
 	{
-		document.body.classList.toggle("playing");
+		play = document.body.classList.toggle("playing") ? switchTo : noop;
 	}
 
 	slides[0].classList.add("active");
@@ -39,7 +34,7 @@
 
 	addEventListener("keypress", function(event)
 	{
-		Maybe({
+		({
 			Escape: toggle,
 			PageUp: prev,
 			PageDown: next,
@@ -47,48 +42,14 @@
 			ArrowDown: next,
 			ArrowLeft: prev,
 			ArrowRight: next,
-			Home: function() { switchTo(0) },
-			End: function() { switchTo(-1) },
-		}[event.code])();
-
-		console.log(event.code);
-	});
-
-	addEventListener("click", function(event)
-	{
-		var isAnchor = function()
-		{
-			for (var node = event.target; node; node = node.parentElement)
-				if (node instanceof HTMLAnchorElement)
-					return true;
-			return false;
-		}();
-
-		var isInteractive = isAnchor || [
-			HTMLEmbedElement,
-			HTMLObjectElement,
-			HTMLMediaElement,
-			SVGSVGElement,
-			HTMLCanvasElement,
-			HTMLInputElement,
-			HTMLTextAreaElement,
-			HTMLSelectElement,
-			HTMLOptionElement,
-		].some(function(constructor)
-		{
-			return event.target instanceof constructor;
-		});
-
-		var handler = isInteractive ? Nothing : event.shiftKey ? prev : next;
-
-		[handler, Nothing, Nothing, prev, next][event.button]();
+			Home: function() { play(0) },
+			End: function() { play(-1) },
+		}[event.code] || noop)();
 	});
 
 	addEventListener("wheel", function(event)
 	{
 		if (event.deltaMode)
-			[next, prev][event.deltaX + event.deltaY < 0]();
-		else
-			console.log(event.deltaX + event.deltaY);
+			(event.deltaX + event.deltaY < 0 ? prev : next)();
 	});
 })();
