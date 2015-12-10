1ce95231321e04be1a0160c0261aa65ad1dd9228
Chen-Pang He <jdh8@ms63.hinet.net>
2015-12-11 01:29:20 +0800

    Add content to 10.1161/JAHA.113.000270

--- s5.js
+++ s5.js
@@ -2,11 +2,19 @@
 
 (function()
 {
+	function KeyboardEventHandler(dict)
+	{
+		return function(event)
+		{
+			var f = dict[event.code];
+			f && f();
+		}
+	}
+
 	var slides = document.getElementsByClassName("slide");
 	var active = 0;
-	var play;
 
-	function switchTo(index)
+	function play(index)
 	{
 		var length = slides.length;
 
@@ -20,41 +28,26 @@
 		active = index;
 	}
 
-	function noop() {}
 	function prev() { play(active - 1) }
 	function next() { play(active + 1) }
 
-	function toggle()
-	{
-		play = document.body.classList.toggle("playing") ? switchTo : noop;
-	}
-
 	slides[0].classList.add("active");
-	toggle();
 
-	addEventListener("keyup", function(event)
-	{
-		({
-			Escape: toggle,
-			PageUp: prev,
-			PageDown: next,
-			ArrowUp: prev,
-			ArrowDown: next,
-			ArrowLeft: prev,
-			ArrowRight: next,
-			Home: function() { play(0) },
-			End: function() { play(-1) },
-		}[event.code] || noop)();
-	});
-
-	addEventListener("wheel", function(event)
-	{
-		var delta = 0;
-
-		delta = ~~(event.deltaX + event.deltaY);
-		delta = event.deltaMode ? delta : delta >> 6;
-		delta = (delta > 0) - (delta < 0) |0;
-
-		[prev, noop, next, noop][delta + 1 & 3]();
-	});
+	addEventListener("keyup", KeyboardEventHandler({
+		Backspace: prev,
+		Enter: next,
+		Space: next,
+		PageUp: prev,
+		PageDown: next,
+		ArrowUp: prev,
+		ArrowDown: next,
+		ArrowLeft: prev,
+		ArrowRight: next,
+		Home: function() { play(0) },
+		End: function() { play(-1) },
+		KeyH: prev,
+		KeyJ: next,
+		KeyK: prev,
+		KeyL: next,
+	}));
 })();
